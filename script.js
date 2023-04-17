c53b08574838b01c00d8c7ed9447b35e30f4b35f
Josef Hrebec <josefhrebec02@gmail.com>
2023-04-17 15:32:26 +0200

    Add dropdown functionality

--- script.js
+++ script.js
@@ -2,9 +2,5 @@ const dropdown = document.querySelector(".hidden-dropdown");
 const btn = document.querySelector(".dropdown-button");
 
 btn.addEventListener("click", () => {
-  if (dropdown.classList.contains("show")) {
-    dropdown.classList.remove("show");
-  } else {
-    dropdown.classList.add("show");
-  }
+  dropdown.classList.toggle("show");
 });
